a47df3c4d70342a980c2493aff40aa61b90653c5
Arthur Rosa <arthur@rosafamily.net>
2022-11-20 01:28:45 -0800

    Adding some comments

--- src/pages/SetupPage.jsx
+++ src/pages/SetupPage.jsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import { UseAuthClient } from "../clients/AuthClient";
 
 function SetupPage() {
+    // This is a demo on how to get data from the API. UseAuthClient()
+    // provides us with an authenticated client that we can use to get data.
     var httpClient = UseAuthClient();
+    // UseState can store data that persists across re-renders
     var [data, setData] = useState('');
     useEffect(() => {
         async function getData() {
+            // Get the data inside the UseEffect
             const val = await httpClient.get('2022/events')
             const json = await val.json()
+            // Store the data in the UseEffect
             setData(JSON.stringify(json))
         }
         getData()
@@ -16,6 +21,7 @@ function SetupPage() {
     return (
         <>
             <div>This is the setup page</div>
+            {/* Display the data we got */}
             {data}
         </>
     )
