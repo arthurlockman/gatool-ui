c7e9b8fb26cdd5401168700c0362c4dcc2b3bec2
James Lockman <jlockman@adobe.com>
2023-03-13 19:42:42 -0400

    Additional resource for cheat sheet

--- src/pages/CheatsheetPage.jsx
+++ src/pages/CheatsheetPage.jsx
@@ -4,6 +4,8 @@ function CheatsheetPage() {
     return (
         <Container fluid>
         <a href="/cheatsheet/charged-up-cheat-sheet.pdf" download><img src="/cheatsheet/Charged-Up-Cheat-Sheet.png" width="100%" alt="Cheatsheet"></img></a>
+        <div><h3>Here is a very useful summary for the playoffs, provided by Bill Aucoin. <a href="/cheatsheet/2023_GA_TACAIDS.pdf" download>Download PDF.</a></h3>
+        <p><br /></p></div>
         </Container>
     )
 }
