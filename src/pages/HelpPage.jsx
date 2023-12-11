ffe0834ed9125e81a58e3e4c03c2b6c7e44d4865
Arthur Rosa <hello@rthr.me>
2023-12-11 11:14:55 -0800

    Hopefully fixing help

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -3,7 +3,7 @@ function HelpPage() {
         <iframe style={{
             bottom: 0,
             width: "100%", height: "90vh"
-        }} src={process.env.PUBLIC_URL + '/help.html'} title="Help" />
+        }} src={process.env.PUBLIC_URL + '/help-docs.html'} title="Help" />
     )
 }
 
