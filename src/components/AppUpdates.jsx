a5446ea466e55499b2f35171fdce4470821a8716
James Lockman <jlockman@adobe.com>
2024-02-25 20:50:27 -0500

    FlashCard updates
    Fixing an issue where flash cards got tall and skinny on a small screen
    Hiding flash cards when there is no event selected
    Closes #265

--- src/components/AppUpdates.jsx
+++ src/components/AppUpdates.jsx
@@ -1,6 +1,68 @@
 
 export const appUpdates = [
     {
+        date: "February 25, 2024",
+        message: <ul>
+            <li>Added V2 of Bill Aucoin's playoff guide</li>
+            <li>Fixed flashcard size issue on cheatsheet page</li>
+            <li>Hide flashcards when no event is selected</li>
+        </ul>
+    },{
+        date: "February 24, 2024",
+        message: <ul>
+            <li>Added Logout button to the Setup Screen when user has not yet selected an event</li>
+            <li>Added Reload button to the Setup Screen</li>
+        </ul>
+    },{
+        date: "February 23, 2024",
+        message: <ul>
+            <li>Update to Bill Aucoin's playoff guide, which is available on the Cheat Sheet screen</li>
+        </ul>
+    },{
+        date: "February 22, 2024",
+        message: <ul>
+            <li>Change how PDF documents download to better support iOS devices</li>
+        </ul>
+    },{
+        date: "February 21, 2024",
+        message: <ul>
+            <li>Updates to Emcee Page playoff display to reduce overset on small screens</li>
+            <li>Adding Rich Text editor to notes fields on the Team Details page</li>
+        </ul>
+    },{
+        date: "February 20, 2024",
+        message: <ul>
+            <li>Many updates to Test Match setup screen</li>
+            <li>Added robot images to flash cards on the Cheat Sheet</li>
+        </ul>
+    },{
+        date: "February 18, 2024",
+        message: <ul>
+            <li>Fixed Week 0 filter on Setup Screen</li>
+        </ul>
+    },{
+        date: "January 23, 2024",
+        message: <ul>
+            <li>Updated Playoff tiebreaker criteria for Crescendo</li>
+            <li>Adding Rich Text editor to notes fields on the Team Details page</li>
+        </ul>
+    },{
+        date: "January 12, 2024",
+        message: <ul>
+            <li>Adjustments to Team Info form to optimize vertical space</li>
+        </ul>
+    },{
+        date: "January 8, 2024",
+        message: <ul>
+            <li>Updated Cheat Sheets for Crescendo</li>
+            <li>Added option to keep event details up to date automatically</li>
+        </ul>
+    },{
+        date: "December 13, 2023",
+        message: <ul>
+            <li>Updated Practice Match schedule to always happen "today"</li>
+        </ul>
+    },{
         date: "December 11, 2023",
         message: <ul>
             <li>Updated to include 2024 season</li>
