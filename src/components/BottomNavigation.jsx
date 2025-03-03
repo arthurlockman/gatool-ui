dd30086e0ac826a4e3dbfd2588c8dd28ca69f645
James Lockman <jlockman@adobe.com>
2025-03-02 20:05:24 -0500

    Update BottomNavigation.jsx

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -1,11 +1,79 @@
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "react-bootstrap/Navbar";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "react-bootstrap";
+import { useState } from "react";
 
 function BottomNavigation() {
-    return <Navbar fixed="bottom" bg="light">
-    <span style={{width: '100%', fontSize: '11px'}}>
-    © 2017-{new Date().getFullYear()} Arthur Rosa &amp; James Lockman, <a target="_blank" href="http://www.northernforce.org/" rel="noreferrer">FIRST Robotics Team 172 Northern Force</a> and the <a target="_blank" href="http://gofarmaine.org" rel="noreferrer">Gorham-Falmouth Alliance for Robotics.</a> <a target="_blank" href="https://frc-events.firstinspires.org/services/API" rel="noreferrer">Event Data provided by <i><b>FIRST</b></i>.</a>
-    </span>
-    </Navbar>;
+  const [url, setURL] = useState({ title: null, url: null });
+  const handleClose = () => {
+    setURL({ title: null, url: null });
+  };
+
+  return (
+    <>
+      <Navbar fixed="bottom" bg="light">
+        <span style={{ width: "100%", fontSize: "11px" }}>
+          © 2017-{new Date().getFullYear()} Arthur Rosa &amp; James Lockman,{" "}
+          <span
+            style={{ cursor: "pointer", color: "blue" }}
+            onClick={() => {
+              setURL({
+                title: "Northern Force",
+                url: "http://www.northernforce.org/",
+              });
+            }}
+          >
+            FIRST Robotics Team 172 Northern Force
+          </span>{" "}
+          and the{" "}
+          <span
+            style={{ cursor: "pointer", color: "blue" }}
+            onClick={() => {
+              setURL({ title: "GoFAR", url: "http://gofarmaine.org" });
+            }}
+          >
+            Gorham-Falmouth Alliance for Robotics.
+          </span>{" "}
+          <span
+            style={{ cursor: "pointer", color: "blue" }}
+            onClick={() => {
+              setURL({
+                title: "FIRST API",
+                url: "https://frc-events.firstinspires.org/services/API",
+              });
+            }}
+          >
+            Event Data provided by{" "}
+            <i>
+              <b>FIRST</b>
+            </i>
+            .
+          </span>
+        </span>
+      </Navbar>
+      <Modal centered={true} show={url?.title} onHide={handleClose} fullscreen>
+        <ModalHeader>{url?.title}</ModalHeader>
+        <ModalBody>
+          <div style={{ height: "calc(100vh - 200px)" }}>
+            <iframe
+              width="100%"
+              height="100%"
+              src={url?.url}
+              title={url?.title}
+            />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={handleClose}>Close {url?.title}</Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
 }
 
 export default BottomNavigation;
