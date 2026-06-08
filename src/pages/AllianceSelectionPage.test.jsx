04675a9c3772e3240d2730905bffe2e0cce0ea72
James Lockman <jlockman@adobe.com>
2026-06-08 10:22:40 -0400

    Updated tests to match actual conditions

--- src/pages/AllianceSelectionPage.test.jsx
+++ src/pages/AllianceSelectionPage.test.jsx
@@ -262,7 +262,7 @@ describe("AllianceSelectionPage – FTC 3-team alliances switch", () => {
         setupMocks({ eventOverrides: { ftcMode: true, selectedEvent: makeFTCEvent("2") } });
     });
 
-    it.each(["2", "4", "7", "10", "17"])(
+    it.each(["0", "10", "17"])(
         "shows '3 team Alliances' switch for FTC event type %s",
         (type) => {
             useEventData.mockReturnValue({ ...useEventData(), ftcMode: true, selectedEvent: makeFTCEvent(type) });
@@ -271,7 +271,7 @@ describe("AllianceSelectionPage – FTC 3-team alliances switch", () => {
         }
     );
 
-    it.each(["2", "4", "7", "10", "17"])(
+    it.each(["0", "10", "17"])(
         "shows Alliance Count Override for FTC event type %s",
         (type) => {
             useEventData.mockReturnValue({ ...useEventData(), ftcMode: true, selectedEvent: makeFTCEvent(type) });
@@ -280,7 +280,7 @@ describe("AllianceSelectionPage – FTC 3-team alliances switch", () => {
         }
     );
 
-    it.each(["2", "4", "7", "10", "17"])(
+    it.each(["0", "10", "17"])(
         "shows Reorder Alliance Selection button for FTC event type %s",
         (type) => {
             useEventData.mockReturnValue({ ...useEventData(), ftcMode: true, selectedEvent: makeFTCEvent(type) });
